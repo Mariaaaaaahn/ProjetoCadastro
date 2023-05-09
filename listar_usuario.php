76fe2addb4b69547ffb8867cc645ce4a00fc8222
Mariaaaaaahn <pontinm758@gmail.com>
2023-05-09 12:10:55 -0300

    inferno

--- listar_usuario.php
+++ listar_usuario.php
@@ -30,6 +30,10 @@
             echo"<td>".$row['nome_usuario']."</td>";
             echo"<td>".$row['email_usuario']."</td>";
             echo"<td>".$row['fone_usuario']."</td>";
+            echo"<td><a href='altera_usuario.php?id_usuario=".$row['id_usuario']."'>Alterar</a></td>";
+
+
+
             echo "</tr>";
 
         } while($row = mysqli_fetch_array($result))
