a9d2f1eeff5d5c5580273e96045aa7635f49f4d6
carolinafreitasguimaraes <carolina.guimaraes@aluno.ifsp.edu.br>
2023-05-16 12:06:39 -0300

    teste

--- altera_usuario.php
+++ altera_usuario.php
@@ -1,17 +1,9 @@
 <?php
-<<<<<<< HEAD
-include ('conexao.php');
-$id_usuario = $_GET['id_usuario'];
-$sql = "SELECT * from usuario where id_usuario=$id_usuario";
-$result = mysqli_query($con,$sql);
-$row = mysqli_fetch_array($result);
-=======
     include('conexao.php');
     $id_usuario = $_GET['id_usuario'];
     $sql = "SELECT * FROM usuario where id_usuario=$id_usuario";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result)
->>>>>>> f3e3b52ff05bdf4057553c8271c014333cc67254
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,19 +14,12 @@ $row = mysqli_fetch_array($result);
     <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
-    <h1>Cadastro de Ususario - IFSP</h1>
-    <form action="altera_usuario_exe.php" method="post">
-        <input name="id_usuario" type="hidden"
-            value="<?php echo $row['id_usuario']?>">
-=======
     <!--http://localhost/PRW/Banco%20de%20dados/Aulas/altera_usuario?id_usuario=1 -->
     <h1>Cadastro de Usuarios - IFSP</h1>
     <form action="altera_ususario_exe.php" method="POST" >
     <input name="id_usuario" type="hidden"
         value="<?php echo $row ['id_usuario']?>">
 
->>>>>>> f3e3b52ff05bdf4057553c8271c014333cc67254
         <div>
             <label for="nome">Nome</label>
             <input type="text" name="nome" id="nome"
