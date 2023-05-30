adce707cf8a6f2b997cc8f0fade4fa7dadd6f737
Mariaaaaaahn <pontinm758@gmail.com>
2023-05-30 12:14:08 -0300

    if

---
+++ fotos/altera_usuario_exe.php
@@ -0,0 +1,31 @@
+<?php
+include('conexao.php');
+
+    $pasta_destino = 'fotos/';
+    $extensao = strtolower(substr($_FILES['foto']['name'],-4));
+    $nome_foto = $pasta_destino.date("Ymd-His"). $extensao;
+    move_uploaded_file($_FILES['foto']['tmp_name'], $nome_foto);
+
+    $id_usuario = $_POST['id_usuario'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $fone = $_POST['fone'];
+    $senha = $_POST['senha'];
+
+    echo "<h1> Alteração de dados </h1>";
+    echo "<p>Usuário: $nome</p>";
+    $sql = "UPDATE usuario SET 
+        nome_usuario = '$nome',
+        email_usuario = '$email',
+        fone_usuario = '$fone',
+        senha = '$senha',
+        foto = '$nome_foto'
+        WHERE id_usuario = $id_usuario";
+    echo $sql."<br>";
+    $result = mysqli_query($con, $sql);
+    if ($result)
+        echo "dados alterados com sucesso!<br>";
+    else 
+        echo "Erro ao alterar dados: ".$msqli_erro($con)."!";
+?>
+<a href="index.php">voltar</a>
